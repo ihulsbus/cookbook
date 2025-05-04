4556eb939bba1e439704da3bc8f6230b75e83dae
Ian Hulsbus <ian@hulsbus.be>
2025-05-04 15:18:24 +0200

    path correction

--- ingredient-service/internal/ingredient-service/ingredient-service.go
+++ ingredient-service/internal/ingredient-service/ingredient-service.go
@@ -30,7 +30,7 @@ func IngredientService(ctx context.Context) {
 
 	v2 := router.Group("/api/v2")
 	{
-		amount := v2.Group("amounts")
+		amount := v2.Group("amount")
 		{
 			readAmounts := amount.Group("")
 			readAmounts.Use(c.KeycloakModule.Middleware("administrator"))
