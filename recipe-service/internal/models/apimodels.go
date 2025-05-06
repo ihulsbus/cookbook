c47a5539a802b14048a2e211ffcd7115a96984a4
Ian Hulsbus <ian@hulsbus.be>
2025-05-06 23:23:46 +0200

    add json tag to recipe object

--- recipe-service/internal/models/apimodels.go
+++ recipe-service/internal/models/apimodels.go
@@ -38,10 +38,10 @@ func (c Recipe) ConvertAllToDTO(recipes []Recipe) []RecipeDTO {
 }
 
 type RecipeDTO struct {
-	ID           uuid.UUID
-	Name         string `gorm:"not null" json:"name" example:"apple pie"`
-	Description  string `gorm:"size:65535;not null" json:"description" example:"pie with apples"`
-	ServingCount int    `gorm:"default:0" json:"servingcount" example:"4"`
+	ID           uuid.UUID `json:"id" example:"23582396-12a3-425b-a597-8a22052823da"`
+	Name         string    `gorm:"not null" json:"name" example:"apple pie"`
+	Description  string    `gorm:"size:65535;not null" json:"description" example:"pie with apples"`
+	ServingCount int       `gorm:"default:0" json:"servingcount" example:"4"`
 }
 
 func (r RecipeDTO) ConvertFromDTO() Recipe {
